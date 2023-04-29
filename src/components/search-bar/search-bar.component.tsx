6d77183ccbc64922e8286b1ba2a0e0b0bf3571c2
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-04-29 15:22:45 -0300

    Rename file to fit a more organized way of modularization

--- src/components/search-bar/search-bar.component.tsx
+++ src/components/search-bar/search-bar.component.tsx
@@ -1,14 +1,10 @@
-import React, { ChangeEventHandler, useState } from "react";
+import React, { useState } from "react";
 
 const SearchBar = (): React.ReactElement => {
   const [searchInput, setSearchInput] = useState("");
   const handleSearchInputChange = (event: React.SyntheticEvent): void => {
-    event.preventDefault();
-
     let target = event.target as HTMLInputElement;
     setSearchInput(target.value);
-
-    console.log(searchInput);
   };
 
   return (
